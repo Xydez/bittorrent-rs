0390a40da7d48842e335ba8954602d920e3fbb24
Xydez <thexydez@gmail.com>
2021-12-28 15:28:20 +0100

    Fix main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use bittorrent::{metainfo::MetaInfo, session::Session};
 async fn main() {
 	let mut session = Session::new(['x' as u8; 20]);
 	
-	let meta = MetaInfo::load("[Ohys-Raws] Kimetsu no Yaiba Yuukaku Hen - 04 (CX 1280x720 264 AAC).mp4.torrent").unwrap();
-	// let meta = MetaInfo::load("debian-10.10.0-amd64-DVD-1.iso.torrent").unwrap();
+	let meta = MetaInfo::load("debian-10.10.0-amd64-DVD-1.iso.torrent").unwrap();
 	println!("== {} ==", meta.name);
 	println!("{:<16}{}", "tracker", meta.announce);
 	println!("{:<16}{}", "pieces", meta.pieces.len());
@@ -15,30 +14,9 @@ async fn main() {
 	}
 	println!();
 
-	// let tracker = Tracker::new(&meta.announce);
-
-	// let response = tracker.announce(&Announce {
-	// 	info_hash: meta.info_hash,
-	// 	peer_id: PEER_ID,
-	// 	ip: None,
-	// 	port: 8000,
-	// 	uploaded: 0,
-	// 	downloaded: 0,
-	// 	left: 0,
-	// 	event: None
-	// }).await.unwrap();
-
-	// println!("{:#?}", response);
-
-	// SocketAddrV4::from_str(...).unwrap()
-	// let peer = Peer::connect(response.peers_addrs[0], &meta, &PEER_ID).await.unwrap();
-	// println!("{:#?}", peer);
-
 	session.add(meta.clone()).await;
 
 	loop {
-		// println!("poll_events");
 		session.poll_events().await;
-		// tokio::time::sleep(Duration::from_secs_f64(0.5)).await;
 	}
 }
