240d8ffcd5eb678462a9d183f6772e8f92f90668
Xydez <thexydez@gmail.com>
2023-07-02 11:51:26 +0200

    Split session handler into functions

--- src/session/handler.rs
+++ src/session/handler.rs
@@ -40,125 +40,14 @@ async fn handle_torrent_event(
 			info!("Torrent announced (torrent_id={torrent_id})");
 		}
 		TorrentEvent::PeerEvent(_peer, PeerEvent::BlockReceived(piece, block)) => {
-			let torrent = &session.torrents[&torrent_id].torrent;
-
-			let Some(download) = torrent.lock().await.state().downloads.get(piece).cloned() else {
-				// This probably means that the download succeeded but we did not cancel the piece
-				warn!("{prefix} | Download for block {} not found, block downloaded in vain", util::fmt_block(*piece, *block));
-				return;
-			};
-
-			let download_lock = download.lock().await;
-
-			if let Some(data) = download_lock.data() {
-				// Remove the piece download because it is done
-				{
-					let mut lock = torrent.lock().await; // TODO: If we decide to not use arc mutex for piecedownload then we can remove this
-					lock.state_mut().downloads.remove(piece);
-
-					//lock.state_mut().pieces[*piece as usize].state = piece::State::Verifying;
-				}
-
-				debug!(
-					"{prefix} | block={} | Final block received, emitting TorrentEvent::Downloaded",
-					util::fmt_block(*piece, *block)
-				);
-
-				session
-					.tx
-					.send(Event::TorrentEvent(
-						torrent_id,
-						TorrentEvent::PieceEvent(*piece, PieceEvent::Downloaded(Arc::new(data)))
-					))
-					.unwrap();
-			}
+			handle_block_received(&prefix, session, torrent_id, *piece, *block).await;
 		}
 		TorrentEvent::PieceEvent(piece, event) => match event {
 			PieceEvent::Downloaded(data) => {
-				debug!("{prefix} | Piece downloaded, starting verification worker");
-
-				let semaphore = session.verification_semaphore.clone();
-				let tx = session.tx.clone();
-				let piece = *piece;
-				let torrent = session.torrents[&torrent_id].torrent.clone();
-				let data = data.clone();
-
-				{
-					let state = torrent.lock().await.state().pieces[piece as usize].state;
-
-					if state != piece::State::Pending {
-						log::warn!(
-							"Piece {piece} downloaded with non-pending state (state = {state:?}"
-						);
-						return;
-					}
-				}
-
-				tokio::spawn(async move {
-					let _permit = semaphore.acquire_owned().await.unwrap();
-					torrent.lock().await.state_mut().pieces[piece as usize].state =
-						piece::State::Verifying;
-
-					let hash = torrent.meta_info.pieces[piece as usize];
-					let intact = async_verify(data.clone(), &hash).await;
-
-					if intact {
-						if tx
-							.send(Event::TorrentEvent(
-								torrent.id,
-								TorrentEvent::PieceEvent(piece, PieceEvent::Verified(data))
-							))
-							.is_err()
-						{
-							panic!("Failed to send TorrentEvent(")
-						}
-					} else {
-						torrent.lock().await.state_mut().pieces[piece as usize].state =
-							piece::State::Pending;
-					}
-				});
+				handle_piece_downloaded(&prefix, session, *piece, torrent_id, data.clone()).await;
 			}
 			PieceEvent::Verified(data) => {
-				debug!("{prefix} | Piece verified, writing to store");
-
-				// Store the piece
-				let tx = session.tx.clone();
-				let data = data.clone();
-				let torrent = session.torrents[&torrent_id].torrent.clone();
-				let piece = *piece;
-
-				if tx.is_closed() {}
-
-				assert!(
-					torrent.lock().await.state().pieces[piece as usize].state
-						== piece::State::Verifying
-				);
-
-				// TODO: (Maybe) push the data onto an io writer queue or similar?
-				tokio::spawn(async move {
-					torrent.lock().await.state_mut().pieces[piece as usize].state =
-						piece::State::Writing;
-
-					{
-						let torrent = torrent.clone();
-
-						tokio::task::spawn_blocking(move || {
-							torrent
-								.store
-								.blocking_lock()
-								.set(piece as usize, &data)
-								.expect("Failed to write to store")
-						})
-						.await
-						.unwrap();
-					}
-
-					tx.send(Event::TorrentEvent(
-						torrent.id,
-						TorrentEvent::PieceEvent(piece, PieceEvent::Done)
-					))
-					.unwrap();
-				});
+				handle_piece_verified(&prefix, session, torrent_id, *piece, data.clone()).await;
 			}
 			PieceEvent::Done => {
 				let torrent = session.torrents[&torrent_id].torrent.clone();
@@ -179,6 +68,133 @@ async fn handle_torrent_event(
 		}
 		_ => ()
 	}
+}
+
+async fn handle_piece_downloaded(
+	prefix: &str,
+	session: &Session,
+	piece: u32,
+	torrent_id: TorrentId,
+	data: Arc<Vec<u8>>
+) {
+	debug!("{prefix} | Piece downloaded, starting verification worker");
+
+	let semaphore = session.verification_semaphore.clone();
+	let tx = session.tx.clone();
+	let torrent = session.torrents[&torrent_id].torrent.clone();
+
+	let state = torrent.lock().await.state().pieces[piece as usize].state;
+
+	if state != piece::State::Pending {
+		log::warn!("Piece {piece} downloaded with non-pending state (state = {state:?}");
+		return;
+	}
+
+	tokio::spawn(async move {
+		let _permit = semaphore.acquire_owned().await.unwrap();
+		torrent.lock().await.state_mut().pieces[piece as usize].state = piece::State::Verifying;
+
+		let hash = torrent.meta_info.pieces[piece as usize];
+		let intact = async_verify(data.clone(), &hash).await;
+
+		if intact {
+			if tx
+				.send(Event::TorrentEvent(
+					torrent.id,
+					TorrentEvent::PieceEvent(piece, PieceEvent::Verified(data))
+				))
+				.is_err()
+			{
+				panic!("Failed to send TorrentEvent(")
+			}
+		} else {
+			torrent.lock().await.state_mut().pieces[piece as usize].state = piece::State::Pending;
+		}
+	});
+}
+
+async fn handle_block_received(
+	prefix: &str,
+	session: &Session,
+	torrent_id: TorrentId,
+	piece: u32,
+	block: u32
+) {
+	let torrent = &session.torrents[&torrent_id].torrent;
+
+	let Some(download) = torrent.lock().await.state().downloads.get(&piece).cloned() else {
+		// This probably means that the download succeeded but we did not cancel the piece
+		warn!("{prefix} | Download for block {} not found, block downloaded in vain", util::fmt_block(piece, block));
+		return;
+	};
+
+	let download_lock = download.lock().await;
+
+	if let Some(data) = download_lock.data() {
+		// Remove the piece download because it is done
+		{
+			let mut lock = torrent.lock().await; // TODO: If we decide to not use arc mutex for piecedownload then we can remove this
+			lock.state_mut().downloads.remove(&piece);
+
+			//lock.state_mut().pieces[*piece as usize].state = piece::State::Verifying;
+		}
+
+		debug!(
+			"{prefix} | block={} | Final block received, emitting TorrentEvent::Downloaded",
+			util::fmt_block(piece, block)
+		);
+
+		session
+			.tx
+			.send(Event::TorrentEvent(
+				torrent_id,
+				TorrentEvent::PieceEvent(piece, PieceEvent::Downloaded(Arc::new(data)))
+			))
+			.unwrap();
+	}
+}
+
+async fn handle_piece_verified(
+	prefix: &str,
+	session: &Session,
+	torrent_id: TorrentId,
+	piece: u32,
+	data: Arc<Vec<u8>>
+) {
+	debug!("{prefix} | Piece verified, writing to store");
+
+	// Store the piece
+	let tx = session.tx.clone();
+	let torrent = session.torrents[&torrent_id].torrent.clone();
+
+	if tx.is_closed() {}
+
+	assert!(torrent.lock().await.state().pieces[piece as usize].state == piece::State::Verifying);
+
+	// TODO: (Maybe) push the data onto an io writer queue or similar?
+	tokio::spawn(async move {
+		torrent.lock().await.state_mut().pieces[piece as usize].state = piece::State::Writing;
+
+		{
+			let torrent = torrent.clone();
+
+			tokio::task::spawn_blocking(move || {
+				torrent
+					.store
+					.blocking_lock()
+					.set(piece as usize, &data)
+					.expect("Failed to write to store")
+			})
+			.await
+			.unwrap();
+		}
+
+		tx.send(Event::TorrentEvent(
+			torrent.id,
+			TorrentEvent::PieceEvent(piece, PieceEvent::Done)
+		))
+		.unwrap();
+	});
 }
 
 /// Computes the Sha1 hash of the piece and compares it to the specified hash, returning whether there is a match
